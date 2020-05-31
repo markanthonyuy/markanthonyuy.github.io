c76c873443fdaeca703459547424c93f40efb3b6
markanthonyuy <macmac.uy@gmail.com>
2020-05-31 10:29:28 +0800

    change project to projects, add 404 page

---
+++ pages/404.js
@@ -0,0 +1,143 @@
+import styles from './contact.module.scss'
+import Container from '../components/Container'
+import Nav from '../components/Nav'
+import { motion } from 'framer-motion'
+
+export default function Contact() {
+  const container = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: {
+        delayChildren: 0.5,
+        staggerChildren: 0.1,
+      },
+    },
+  }
+
+  const item = {
+    hidden: { opacity: 0 },
+    show: { opacity: 1 },
+  }
+
+  const email = 'macmac.uy'
+
+  return (
+    <Container>
+      <Nav active="contact" />
+      <div className={styles.contact}>
+        <motion.h1
+          initial={{ y: 50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+        >
+          404! Are you lost?
+        </motion.h1>
+
+        <motion.p
+          initial={{ y: 50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1, transition: { delay: 0.2 } }}
+        >
+          I've been there. You can click the links above to guide your way back.
+        </motion.p>
+
+        <motion.p
+          initial={{ y: 50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1, transition: { delay: 0.2 } }}
+        >
+          Only a few are able to see this page. <b>The Lost</b>,{' '}
+          <b>The Explorers</b>, <b>The Wanderers</b>, <b>The Thinkerers</b>,{' '}
+          <b>The Hackers</b> who constantly look for possible penetration to my
+          code. (Just check out{' '}
+          <a href="https://github.com/markanthonyuy">my Github</a> to make your
+          life easier. 😉) or Maybe you just simply made a typo, let me call you{' '}
+          <b>The Typo Maker!</b>.
+        </motion.p>
+
+        <motion.p
+          initial={{ y: 50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1, transition: { delay: 0.2 } }}
+        >
+          Let me just share to you my life story.
+        </motion.p>
+
+        <motion.h2
+          initial={{ y: 50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1, transition: { delay: 0.3 } }}
+        >
+          My Story
+        </motion.h2>
+
+        <motion.p
+          initial={{ y: 50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1, transition: { delay: 0.4 } }}
+        >
+          My life has been an uphill battle. Living in a third world country
+          isn't easy. I remember the time when we just try to survive each day.
+          My father had a stroke when I was 10 years old and he was the source
+          of our living. By the time I went to study college we really had no
+          money. Luckily my parents set up a college fund for me when they still
+          have money. Then we were forced out of our house. Some of our
+          relatives helped by letting us stay in their houses for a period of
+          time. I'm will be forever grateful for that. This is also the time I
+          suffered from depression. I stopped all my hobbies, I stopped eating,
+          it feels like I lost my purpose. I just want to stop all the pain and
+          adversities and end my life. Then my mom got a stage 3 cancer. Seems
+          like everything is falling into pieces.
+        </motion.p>
+
+        <motion.p
+          initial={{ y: 50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1, transition: { delay: 0.4 } }}
+        >
+          But hey, I survive that, I found my path to redemption. And I know you
+          will too. It's not going to be easy, just hang in there. Feel free to
+          reach out to me anytime if you need someone to talk to.
+        </motion.p>
+
+        <motion.blockquote
+          initial={{ y: 50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1, transition: { delay: 0.4 } }}
+        >
+          <p>”Fall down seven times, stand up eight.” ― Proverb</p>
+        </motion.blockquote>
+
+        <motion.p
+          initial={{ y: 50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1, transition: { delay: 0.4 } }}
+        >
+          Now I'm happily married. I still can't afford to buy a house, but who
+          knows? One day, I'm sure it'll come. I moved on from all hardships I
+          experience. I still have problems of my own, and sometimes demons from
+          my past still haunt me. But every day, I keep reminding myself that
+          there is light at the end of the tunnel.
+        </motion.p>
+
+        <motion.blockquote
+          initial={{ y: 50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1, transition: { delay: 0.4 } }}
+        >
+          <p>
+            ”As I walk through the valley of the shadow of death, I will fear no
+            evil.” ― Psalm 23:4
+          </p>
+        </motion.blockquote>
+
+        <motion.p
+          initial={{ y: 50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1, transition: { delay: 0.4 } }}
+        >
+          And always remember.
+        </motion.p>
+
+        <motion.blockquote
+          initial={{ y: 50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1, transition: { delay: 0.4 } }}
+        >
+          <p>
+            ”What Doesn't Kill You Makes You Stronger.” ― German Philosopher
+          </p>
+        </motion.blockquote>
+      </div>
+    </Container>
+  )
+}
---
+++ pages/projects.js
@@ -0,0 +1,206 @@
+import styles from './project.module.scss'
+import Container from '../components/Container'
+import Nav from '../components/Nav'
+import { motion } from 'framer-motion'
+
+export default function Project() {
+  const container = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: {
+        delay: 0.7,
+        delayChildren: 0.3,
+        staggerChildren: 0.1,
+      },
+    },
+  }
+
+  const item = {
+    hidden: { opacity: 0 },
+    show: { opacity: 1 },
+  }
+
+  const projects = [
+    {
+      name: 'Personal Website',
+      link: 'https://markanthonyuy.com/',
+      desc: 'This is the version 2 personal website.',
+      uses: ['Nextjs', 'Framer'],
+    },
+    {
+      name: 'Bingoo',
+      link: 'https://bingoo.now.sh/',
+      desc: 'Fun to play Bingo Game.',
+      uses: ['Reactjs', 'Framer', 'Styled-Components'],
+    },
+    {
+      name: 'Covid 19 Philippines Tracker',
+      link: 'https://covid19.com.ph/',
+      desc: 'Dedicated Covid 19 Tracker for Philippines.',
+      uses: ['Reactjs', 'Tailwind'],
+    },
+    {
+      name: 'Philippine UITF Listing',
+      link: 'https://uitf.now.sh/',
+      desc: "Updated market value of UITF's available in the Philippines.",
+      uses: ['Reactjs', 'Material UI', 'PWA'],
+    },
+    {
+      name: 'Cryptomark',
+      link: 'https://cryptomark.now.sh/',
+      desc: 'Compare Cryptocurrencies Realtime.',
+      uses: ['Reactjs', 'Bulma'],
+    },
+    {
+      name: 'Pokemon DB',
+      link: 'https://pokedb.now.sh/',
+      desc: 'Pokemon Database.',
+      uses: ['Nextjs', 'Framer', 'Sass'],
+    },
+    {
+      name: 'Dota 2 Leaderboards',
+      link: 'https://dota2leaderboards.now.sh/',
+      desc: 'List of Dota2 players with the highest Rank.',
+      uses: ['Nextjs', 'Framer', 'Serverless'],
+    },
+    {
+      name: 'Dota 2 Earnings',
+      link: 'https://dota2earnings.now.sh/',
+      desc: 'List of Dota 2 professional players with the highest earnings.',
+      uses: ['Nextjs', 'Framer', 'Serverless'],
+    },
+    {
+      name: 'Palitan',
+      link: 'https://markanthonyuy.com/palitan/',
+      desc: 'Simple currency exchange rate.',
+      uses: ['Reactjs'],
+    },
+    {
+      name: 'Manga Libre',
+      link: 'https://mangalibre.herokuapp.com/',
+      desc: 'Free, fast, simple and straightforward Manga Reader.',
+      uses: ['Backbonejs', 'Jquery'],
+    },
+    {
+      name: 'Libre Manga',
+      link: 'https://libremanga.herokuapp.com/',
+      desc: 'My first manga reader app.',
+      uses: ['HTML', 'CSS', 'Jquery'],
+    },
+    {
+      name: 'Marktip',
+      link: 'https://markanthonyuy.com/markTip/',
+      desc: 'Jquery tootlip plugin.',
+      uses: ['Javascript', 'Jquery'],
+    },
+    {
+      name: 'Randomizer',
+      link: 'https://markanthonyuy.com/randomizer/',
+      desc: 'Random Team Generator.',
+      uses: ['HTML', 'CSS', 'Javascript'],
+    },
+  ]
+
+  return (
+    <Container>
+      <Nav active="project" />
+      <div className={styles.project}>
+        <motion.h1
+          initial={{ y: 50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+        >
+          My Work
+        </motion.h1>
+
+        <motion.h2
+          initial={{ y: 50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1, transition: { delay: 0.2 } }}
+        >
+          Professional
+        </motion.h2>
+
+        <motion.p
+          initial={{ y: 50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1, transition: { delay: 0.3 } }}
+        >
+          I'm currently maintaining and developing an ecommerce site.
+        </motion.p>
+
+        <motion.h2
+          initial={{ y: 50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1, transition: { delay: 0.4 } }}
+        >
+          Personal
+        </motion.h2>
+
+        <motion.p
+          initial={{ y: 50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1, transition: { delay: 0.5 } }}
+        >
+          During my pastime I like to think of how can I solve problems. Just
+          simple problems that I'm having. I remember a colleague of mine first
+          made a Manga reader. I was in awe and got really inspired from that
+          day. Since I love reading manga, I told myself that what is the
+          problem that I mostly encounter. The answers are LOADING SPEED, POOR
+          UI and Ads everywhere! If you're reading manga definitely you are
+          aware of these problems, besides manga images are extremely huge the
+          sites are always bombarded with ads. Quite annoying. I don't blame
+          them though if that's their monetization strategy. Anyway, I just took
+          action on the matter and build my own Manga Reader. And{' '}
+          <a href="http://libremanga.herokuapp.com/">Libremanga</a> was born.
+          (FYI this is not maintained.) My first web app. Then the rest is
+          history :)
+        </motion.p>
+
+        <motion.blockquote
+          initial={{ y: 50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1, transition: { delay: 0.5 } }}
+        >
+          <p>
+            “A problem is a chance for you to do your best.” ― Duke Ellington
+          </p>
+        </motion.blockquote>
+
+        <motion.p
+          initial={{ y: 50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1, transition: { delay: 0.5 } }}
+        >
+          I like to use different tech/tools when creating my own app. Mix and
+          match to see which will be the best fit for the requirements and my
+          style. This is one of the great parts of doing your own projects. You
+          get to decide which one to use. I'm my own BOSS!
+        </motion.p>
+
+        <motion.p
+          initial={{ y: 50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1, transition: { delay: 0.5 } }}
+        >
+          Here are some of my personal projects. Or you can check out my{' '}
+          <a href="https://github.com/markanthonyuy">github account</a>.
+        </motion.p>
+
+        <motion.div
+          className={styles.list}
+          variants={container}
+          initial="hidden"
+          animate="show"
+        >
+          {projects.map((project) => (
+            <motion.div variants={item} key={project.name}>
+              <a href={project.link} className={styles.title}>
+                {project.name}
+              </a>
+              <span className={styles.desc}>{project.desc}</span>
+              <span className={styles.built}>
+                {project.uses.map((u, key) => (
+                  <span key={`${u}-${key}`}>{u}</span>
+                ))}
+              </span>
+            </motion.div>
+          ))}
+        </motion.div>
+      </div>
+    </Container>
+  )
+}
