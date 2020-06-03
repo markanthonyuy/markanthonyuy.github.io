74bbf1c487a944eec97635bd4857ec25af4b9992
markanthonyuy <macmac.uy@gmail.com>
2020-06-03 18:58:46 +0800

    add new build

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b7661795eeb2c2d89982";
+/******/ 	var hotCurrentHash = "6054db9f97c5ac4e7f03";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
