cb2fb0d3c7b231d90d6e21029e820217d0732a13
markanthonyuy <macmac.uy@gmail.com>
2020-06-12 18:07:53 +0800

    fix sentences, use 3 columns for list

--- pages/about.js
+++ pages/about.js
@@ -55,7 +55,7 @@ export default function About() {
   ]
 
   const makeList = ({ list, title }) => (
-    <>
+    <div className={styles.box}>
       <motion.h3
         initial={{ y: 50, opacity: 0 }}
         animate={{ y: 0, opacity: 1, transition: { delay: 1 } }}
@@ -68,13 +68,13 @@ export default function About() {
       >
         {list.map((p, key) => (
           <li key={`${p.name}-${key}`}>
-            <a href={p.link} rel="noopener">
+            <a href={p.link} rel="noopener" target="_blank">
               {p.name}
             </a>
           </li>
         ))}
       </motion.ul>
-    </>
+    </div>
   )
 
   return (
@@ -149,10 +149,11 @@ export default function About() {
           I've been web developing for 8+ years now. Time does indeed fly when
           you're having fun. It seems only yesterday when I started converting
           PSD's to HTML as my first web developer job. Then I became a Software
-          Engineer in the BPO industry. I got to work with various companies
-          there. I work mostly on front-end development because that's where I
-          found my passion. I love seeing fast, creative and intuitive websites.
-          Google lighthouse is my friend.
+          Engineer in the BPO industry. That's where I got expose with various
+          companies big companies. Like Globe and TV5 Network. I work mostly on
+          front-end development because that's where I found my passion. I love
+          seeing fast, creative and intuitive websites. Google lighthouse is my
+          friend.
         </motion.p>
 
         <motion.blockquote
@@ -173,11 +174,11 @@ export default function About() {
           initial={{ y: 50, opacity: 0 }}
           animate={{ y: 0, opacity: 1, transition: { delay: 0.4 } }}
         >
-          I'm very passionate at what I do even though it's not easy keeping up
+          I'm very passionate at what I do, even though it's not easy keeping up
           with the technologies. It feels impossible to learn everything but if
           there's one thing I learn from using Linux, it's{' '}
           <b>Do one thing and do it really good</b>. That's the reason I focus
-          in the front-end side.
+          on the front-end side.
         </motion.p>
 
         <motion.p
@@ -216,8 +217,27 @@ export default function About() {
           initial={{ y: 50, opacity: 0 }}
           animate={{ y: 0, opacity: 1, transition: { delay: 0.6 } }}
         >
-          I'm a big fan of open source. I use the following languages,
-          libraries, frameworks and tools when creating a website.
+          Big fan of open source. I try to use them all the time with my
+          projects. It first happened when I was forced to develop in a Linux
+          machine. Ubuntu to be precise. It was daunting at first, Setting up
+          virtual host, Editing files using nano or vim, Just using
+          terminal/command line is very intimidating for a noob like myself. I
+          remember when I was stuck inside VIM for almost an hour. LOL (damn{' '}
+          <code>:q!</code>) It took me quite a while to adjust from using
+          Windows to Linux. But once you put your foot on that water there is no
+          turning back. To be honest, I can't see myself developing on a Windows
+          machine again. Not that I have anything against Windows. It's just
+          that I'm so comfortable with Linux right now I'm just look for the
+          best distro's to try out there.
+        </motion.p>
+
+        <motion.p
+          initial={{ y: 50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1, transition: { delay: 0.6 } }}
+        >
+          {' '}
+          The following are languages, libraries, frameworks and tools that I
+          use when creating a website.
         </motion.p>
 
         <ToolIcons delay={0.7} />
@@ -234,11 +254,18 @@ export default function About() {
         >
           This new design is inspired by my favorite websites. I often got my
           ideas from looking at other people work, then I add my own flavor and
-          style to the mix. For example the colored border that wrapped the
-          quotes is inspired by Codepen's button, Bigger fonts is from Smashing
-          magazine and Design course website. Hopefully, I was able to create
-          something cool. If you want a peek of my old design here is the{' '}
-          <a href="https://markanthonyuy.com/old">link</a> .
+          style to the mix. For example, the colored border that wrapped the
+          quotes is inspired by Codepen's button, Big fonts and spacious
+          elements are from Smashing magazine and Design course website. I feel
+          really good and proud of the outcome. It's way far from my previous
+          design.
+        </motion.p>
+        <motion.p
+          initial={{ y: 50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1, transition: { delay: 0.9 } }}
+        >
+          Here is the <a href="https://markanthonyuy.com/old">link</a> if you
+          want a peek.
         </motion.p>
 
         <motion.blockquote
@@ -260,9 +287,11 @@ export default function About() {
           here are some.
         </motion.p>
 
-        {makeList({ list: PEOPLE, title: 'People' })}
-        {makeList({ list: PODCAST, title: 'Podcast' })}
-        {makeList({ list: INSPIRATIONS, title: 'Inspirations' })}
+        <div className={styles.list}>
+          {makeList({ list: PEOPLE, title: 'People' })}
+          {makeList({ list: PODCAST, title: 'Podcast' })}
+          {makeList({ list: INSPIRATIONS, title: 'Inspirations' })}
+        </div>
       </div>
     </Container>
   )
