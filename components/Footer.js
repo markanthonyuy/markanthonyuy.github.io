a696585917a551c1cca8323a0f276dc320c9b337
markanthonyuy <macmac.uy@gmail.com>
2020-06-12 18:06:37 +0800

    use js for year

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,11 @@
 import styles from './footer.module.scss'
 
+const year = new Date().getFullYear()
+
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>Mark Anthony Uy &copy; 2020</p>
+      <p>Mark Anthony Uy &copy; {year}</p>
     </footer>
   )
 }
