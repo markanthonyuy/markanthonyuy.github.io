cb2bc74df92f78304858b8c9162aee80c65cdd72
markanthonyuy <macmac.uy@gmail.com>
2020-06-12 18:08:56 +0800

    add jejenator, add icon, add target blank

--- pages/projects.js
+++ pages/projects.js
@@ -23,6 +23,12 @@ export default function Projects() {
 
   const projects = [
     {
+      name: 'Jejenator',
+      link: 'https://jejenator.netlify.app/',
+      desc: 'The Jejemon Generator. Converts regular word to a Jejemon word.',
+      uses: ['Sveltejs'],
+    },
+    {
       name: 'Personal Website',
       link: 'https://markanthonyuy.com/',
       desc: 'This is the version 2 personal website.',
@@ -189,9 +195,16 @@ export default function Projects() {
         >
           {projects.map((project) => (
             <motion.div variants={item} key={project.name}>
-              <a href={project.link} className={styles.title}>
-                {project.name}
-              </a>
+              <div className={styles.top}>
+                <a href={project.link} className={styles.title} target="_blank">
+                  {project.name}
+                </a>
+                <a href={project.link} className={styles.icon} target="_blank">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
+                    <path d="M41.28 10.323h178.774l73.052 82.466H115.194c-17.61 0-32.02 14.41-32.02 32.02v401.403c0 17.598 14.41 32.02 32.02 32.02h406.328c17.61 0 32.02-14.422 32.02-32.02v-181.62l83.174 69.19v185.624c0 22.7-18.567 41.28-41.28 41.28H41.28c-22.712 0-41.28-18.58-41.28-41.28V51.604c0-22.702 18.568-41.28 41.28-41.28zM640-.685L311.378 13.843l104.801 104.8-230.755 230.744 101.21 101.21 230.755-230.755 106.82 106.82L640-.685z" />
+                  </svg>
+                </a>
+              </div>
               <span className={styles.desc}>{project.desc}</span>
               <span className={styles.built}>
                 {project.uses.map((u, key) => (
