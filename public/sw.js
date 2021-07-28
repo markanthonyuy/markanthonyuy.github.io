9eef37c801fd95cf64e46762c916b1ccbc75008f
markanthonyuy <macmac.uy@gmail.com>
2021-07-28 09:21:43 +0800

    update sw

--- public/sw.js
+++ public/sw.js
@@ -118,7 +118,7 @@ define("./sw.js",['./workbox-b90066a8'], function (workbox) { 'use strict';
     "revision": "9d11ea7095b60a828f2db49ed1004ade"
   }, {
     "url": "/_next/static/runtime/webpack.js",
-    "revision": "05690da663cf377036354868bcb1aeb5"
+    "revision": "deaf11fbd33c84620a05892a8566de50"
   }], {
     "ignoreURLParametersMatching": [/ts/]
   });
