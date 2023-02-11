0b743ad76c6d2fbc7633d16e5ba3ca329719c97c
markanthonyuy <macmac.uy@gmail.com>
2023-02-11 10:18:44 +0800

    add uno websites project

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "828fc9ace9ed59ab6af4";
+/******/ 	var hotCurrentHash = "cfd21704049205670874";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
